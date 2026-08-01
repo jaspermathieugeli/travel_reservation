a75197227e40dcbae0c779a9a6b9a011153a9c89
Jasper Mathieu Geli <jaspermathieugeli@gmail.com>
2026-08-01 18:19:18 +0800

    create video background component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
     };
 
   return (
-    <div className='flex justify-between items-center h-20 px-4'>
+    <div className='flex w-full justify-between items-center h-20 px-4 absolute z-10 text-white'>
         <div>
             <h1 onClick={handleNav} className={logo ? 'hidden' : 'block'}>BEACHES</h1>
         </div>
@@ -36,7 +36,7 @@ const Navbar = () => {
         </div>
 
         {/* Mobile Menu */}
-        <div onClick={handleNav} className={nav ? 'absolute left-0 top-0 w-full bg-gray-100/90 px-4 py-7 flex flex-col' : 'absolute left-[-100%]'}>
+        <div onClick={handleNav} className={nav ? 'absolute text-black left-0 top-0 w-full bg-gray-100/90 px-4 py-7 flex flex-col' : 'absolute left-[-100%]'}>
             <div>
                 <h1>BEACHES</h1>
             </div>
