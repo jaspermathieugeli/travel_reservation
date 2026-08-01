52587f128c07f77479786f0b2e5dbab30c523e92
Jasper Mathieu Geli <jaspermathieugeli@gmail.com>
2026-08-01 17:34:36 +0800

    add global styles with Tailwind CSS

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,63 @@
-import React from 'react'
-import {BsPerson} from 'react-icons/bs'
-import {BiSearch} from 'react-icons/bi'
+import React, {useState} from 'react';
+import {BsPerson} from 'react-icons/bs';
+import {BiSearch} from 'react-icons/bi';
+import {AiOutlineClose} from 'react-icons/ai';
+import {HiOutlineMenuAlt4} from 'react-icons/hi';
+import {FaFacebook, FaInstagram, FaTwitter, FaPinterest, FaYoutube} from 'react-icons/fa';
 
 const Navbar = () => {
+    const [nav, setNav] = useState(false);
+    const [logo, setLogo] = useState(false);
+    const handleNav = () => {
+        setNav(!nav);
+        setLogo(!logo);
+    };
+
   return (
     <div className='flex justify-between items-center h-20 px-4'>
         <div>
-            <h1>BEACHES</h1>
+            <h1 onClick={handleNav} className={logo ? 'hidden' : 'block'}>BEACHES</h1>
         </div>
-        <ul className='flex'>
+        <ul className='hidden md:flex'>
             <li>Home</li>
             <li>Destinations</li>
             <li>Travel</li>
             <li>View</li>
             <li>Book</li>
         </ul>
-        <div className='flex'>
-            <BsPerson />
-            <BiSearch />
+        <div className='hidden md:flex'>
+            <BsPerson className='mr-1' size={20} />
+            <BiSearch className='ml-1' size={20} />
+        </div>
+
+        {/* Hamburger */}
+        <div onClick={handleNav} className='md:hidden z-10'>
+            {nav ? <AiOutlineClose className='text-black' size={20} /> : <HiOutlineMenuAlt4 size={20} />}
+        </div>
+
+        {/* Mobile Menu */}
+        <div onClick={handleNav} className={nav ? 'absolute left-0 top-0 w-full bg-gray-100/90 px-4 py-7 flex flex-col' : 'absolute left-[-100%]'}>
+            <div>
+                <h1>BEACHES</h1>
+            </div>
+            <ul>
+                <li className='border-b'>Home</li>
+                <li className='border-b'>Destinations</li>
+                <li className='border-b'>Travel</li>
+                <li className='border-b'>View</li>
+                <li className='border-b'>Book</li>
+                <div className='flex flex-col'>
+                    <button className='my-3'>Search</button>
+                    <button className='my-3'>Account</button>
+                </div>
+                <div className='flex justify-between my-3'>
+                    <FaFacebook className='icon' />
+                    <FaInstagram className='icon' />
+                    <FaTwitter className='icon' />
+                    <FaPinterest className='icon' />
+                    <FaYoutube className='icon' />
+                </div>
+            </ul>
         </div>
     </div>
   )
